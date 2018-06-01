fa824cdd4b1fc68385b893dd1f5edf4632e52e3e
argonavt11 <argonavt.al@yandex.ru>
2018-06-01 23:09:26 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ColoredCalendar from 'components/ColoredCalendar';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <ColoredCalendar />
+      </div>
+    );
+  }
+}
+
+export default App;

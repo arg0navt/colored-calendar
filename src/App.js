723552063e397b7a263d53c1de52adf68eb47707
argonavt11 <argonavt.al@yandex.ru>
2018-06-06 22:03:56 +0300

    feat: single calendar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ColoredCalendar from 'components/ColoredCalendar';
+import { SingleCalendar } from  'components/ColoredCalendar/SingleCalendar'
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <ColoredCalendar />
+        <SingleCalendar />
       </div>
     );
   }
